d4543508cee2365b85d0007716e60073faef03f6
akell <36044882+atkell@users.noreply.github.com>
2020-02-18 18:07:15 -0500

    Creating view and controller for editing a single card

---
+++ popup/edit_card.js
@@ -0,0 +1,23 @@
+document.getElementById("add").addEventListener("click", makeThing);
+document.getElementById("delete").addEventListener("click", clearAllItems);
+
+function makeThing() {
+
+    const newAccomplishment = new Accomplishment(
+        document.getElementById('summary').value,
+        document.getElementById('status').value,
+        document.getElementById('details').value,
+        Date.now()
+    );
+    newAccomplishment.save();
+}
+
+function openInNewTab() {
+    chrome.tabs.create({url: chrome.extension.getURL('popup/view_all.html#window')});
+}
+
+
+function clearAllItems() {
+    chrome.storage.sync.clear();
+    console.log('All items removed.');
+}
