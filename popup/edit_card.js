b9c04f9b00daa06f9d4cc5c895a8f3abce58041a
akell <36044882+atkell@users.noreply.github.com>
2020-02-19 23:02:51 -0500

    Removed old code

--- popup/edit_card.js
+++ popup/edit_card.js
@@ -1,34 +1,3 @@
-// function demo() {
-//
-//     // We're passing the unique key of our entry as a query string parameter
-//     // Remember, our key is a timestamp so this seems sane and safe
-//     // We're also only passing a single query string parameter at this point
-//     // so it also seems sane to only grab the first value from the URL and ignore others
-//     var card_id = document.location.href.split('?')[1].split('=')[1];
-//
-//     // Now reach out to storage with this key
-//     chrome.storage.sync.get([card_id], function (result) {
-//
-//         const storageBox = Object.values(result);
-//         // And set the values associated with this key to the values of the input fields
-//         document.getElementById('summary').value = storageBox[0][0]['summary'];
-//         document.getElementById('status').value = storageBox[0][3]['status'];
-//         document.getElementById('details').value = storageBox[0][1]['details'];
-//
-//         document.getElementById("update").addEventListener("click", function() {
-//             const updatedAccomplishment = new Accomplishment(
-//                 document.getElementById('summary').value,
-//                 document.getElementById('status').value,
-//                 document.getElementById('details').value,
-//                 card_id
-//             );
-//             updatedAccomplishment.save();
-//         });
-//
-//     });
-// }
-
-
 document.addEventListener("DOMContentLoaded", function() {
     // Create a new object with undefined properties
     // Call out to storage to get the values
@@ -44,11 +13,9 @@ document.addEventListener("DOMContentLoaded", function() {
         updatedAccomplishment.status = document.getElementById('status').value;
         updatedAccomplishment.details = document.getElementById('details').value;
         updatedAccomplishment.date = card_id;
-        updatedAccomplishment.save()
+        updatedAccomplishment.save();
     });
-    // updatedAccomplishment.save();
-
 });
 
-// demo();
+
 
