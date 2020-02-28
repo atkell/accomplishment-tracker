da99c1a33ddb641ea33332522618744a534f5e8b
akell <36044882+atkell@users.noreply.github.com>
2020-02-28 00:21:17 -0500

    Caught a bug where the random number was excluding the max

--- popup/view_random.js
+++ popup/view_random.js
@@ -7,7 +7,8 @@
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    // return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
 document.addEventListener("DOMContentLoaded", function() {
