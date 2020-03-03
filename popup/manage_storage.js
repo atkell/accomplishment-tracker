bcbe5bb408156d416ca8b4503efb421f9c7315d2
akell <36044882+atkell@users.noreply.github.com>
2020-03-03 11:48:02 -0500

    Made the remove all button work

--- popup/manage_storage.js
+++ popup/manage_storage.js
@@ -2,4 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const accomplishment = new Accomplishment();
     accomplishment.freeSpace();
     accomplishment.countStoredItems();
+
+    // Remove all
+    document.getElementById("delete-all").addEventListener("click", function () {
+        chrome.storage.sync.clear();
+    });
 });
